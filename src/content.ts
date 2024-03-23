cca288eb86abf4968a60f2f4a57b2f76f491bd2e
s1j1k <sallykalumba@gmail.com>
2024-03-23 19:38:01 +1100

    WIP add text when shortcut detected

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,6 @@
-document.addEventListener("keyup", (event) => {return onKeyUp(event);});
+document.addEventListener("keyup", (event) => {
+  return onKeyUp(event);
+});
 
 // TODO detect certain patterns
 // save the last 4-5 characters and detect if they match any of the pattersn in the shortcuts
@@ -9,82 +11,104 @@ document.addEventListener("keyup", (event) => {return onKeyUp(event);});
 // FIXME should we do with by checking inner HTML itself(?)
 
 function onKeyUp(event: KeyboardEvent): void {
-    // get the text which is just updated
-    const el = document.activeElement
+  // get the text which is just updated
+  const el = document.activeElement;
 
-    if (!el) {
-        return;
+  // if (!el) {
+  //     return;
+  // }
+
+  // only act on space
+  const keyName = event.key;
+  console.log("keyName", keyName);
+
+  if (keyName != " ") {
+    return;
+  }
+
+  console.log("Detected a space");
+
+  // get the text in that element
+  if (!el) {
+    console.log("Element is undefined, returning");
+    return;
+  }
+
+  // just look at the last few chars
+  const BUFFER = 50;
+  const text = el.textContent;
+
+  if (!text) {
+    console.log("No text in active element, returning");
+    return;
+  }
+
+  // slice the text length to fit within a smaller buffer
+  const textBuffer =
+    text.length > BUFFER ? text.slice(text.length - BUFFER) : text;
+
+  console.log("text buffer: ", textBuffer);
+
+  // TODO make sure the last key entered is the last letter of the shortcut
+  console.log("Searching for shortcuts");
+  for (const [key, value] of Object.entries(shortcuts)) {
+    //console.log(`${key}: ${value}`);
+
+    // find the last entry in text
+    // TODO make this more efficient ? only look in the last few chars?
+    const idx = textBuffer?.lastIndexOf(key);
+
+    if (idx === -1) {
+      console.log("Shortcut ", key, " not detected in buffer.");
     }
 
-    // only act on space
-    const keyName = event.key;
-    if (keyName != " ") {
-        return;
+    // make sure it is the very last thing in the element
+    if (idx !== -1 && key === textBuffer?.slice(idx).replace(" ", "")) {
+      console.log("Shortcut ", key, " detected in buffer!");
+      console.log("inserting text", value());
+      // TODO replace with the shortcut value
+      el.insertAdjacentText("beforeend", value());
     }
+  }
 
-    // get the text in that element
-    const text = el?.textContent;
+  // get the most recent element which needs shortcutting
 
+  // console.log("Active element tag name: ",el?.tagName)
 
-    // TODO make sure the last key entered is the last letter of the shortcut
-    for (const [key, value] of Object.entries(shortcuts)) {
-        //console.log(`${key}: ${value}`);
+  // insertAdjacentText("beforeend", textInput.value);
 
-        // find the last entry in text
-        const idx = text?.lastIndexOf(key);
-        
-        // make sure it is the very last thing in the element
-        if (key === text?.slice(idx).replace(" ","")) {
-            console.log("inserting text", value())
-            el.insertAdjacentText("beforeend", value());
-        } 
+  // the whole notion page is being returned
+  //console.log("Text in active element=", text)
 
-        // replace with the shortcut value 
+  // make sure the active element is being updated
 
-    }
+  // console.log(keyName);
 
-    // get the most recent element which needs shortcutting 
-    
-    
-    // console.log("Active element tag name: ",el?.tagName)
+  // // ignore special keys and only append single chars
+  // if (keyName.length == 1) {
+  //     keyBuffer += keyName;
+  // }
 
-    // insertAdjacentText("beforeend", textInput.value);
+  // if (keyName === " " || keyBuffer.length > BUFFER_SIZE ) {
+  //     // clear the buffer after a space or too long an entry
+  //     keyBuffer = ""
+  // }
 
-    // the whole notion page is being returned
-    //console.log("Text in active element=", text)
-    
+  // // check for shortcuts
+  // if (keyBuffer in shortcuts) {
+  //     // append the replacement code
+  //     // TODO include code for changing cursor position
 
-    // make sure the active element is being updated
-
-    // console.log(keyName);
-
-    // // ignore special keys and only append single chars
-    // if (keyName.length == 1) {
-    //     keyBuffer += keyName;
-    // }
-
-    // if (keyName === " " || keyBuffer.length > BUFFER_SIZE ) {
-    //     // clear the buffer after a space or too long an entry
-    //     keyBuffer = ""
-    // } 
-
-    // // check for shortcuts
-    // if (keyBuffer in shortcuts) {
-    //     // append the replacement code
-    //     // TODO include code for changing cursor position
-        
-    // }
-
+  // }
 }
-
 
 // shortcuts
 const shortcuts = {
-    "\beg": () => {
-        // return the shortcut 
-        // TODO capture some of what the user is typing until they hit enter or tab or something
-        // NOTE removed the \beg in \begin
-        // TODO add the environment name
-        return "in{} \end{}"
-    }
+  "\beg": () => {
+    // return the shortcut
+    // TODO capture some of what the user is typing until they hit enter or tab or something
+    // NOTE removed the \beg in \begin
+    // TODO add the environment name
+    return "in{} end{}";
+  },
 };
