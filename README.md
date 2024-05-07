6cb65d38465adfded530cd289e48141b42442ae4
s1j1k <89644676+s1j1k@users.noreply.github.com>
2024-05-07 17:41:49 +1000

    fix typescript errors

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Latex shortcuts chrome extension
 Shortcuts:
 - `\beg` expand to `\begin{} \end{}`
+
+
+## Build
--- src/content.ts
+++ src/content.ts
@@ -7,7 +7,7 @@ const re3 = /\\begin{}(.*)\\end{([a-z]*)}/i;
 enum Token {
   keyword = "token keyword",
   functionSelector = "token function selector",
-  punctuation = "token punctuation"
+  punctuation = "token punctuation",
 }
 
 // check for shortcut pattern after any key is released
@@ -74,59 +74,57 @@ function onKeyUp(event: KeyboardEvent): void {
   // TODO convert $$ to inline math mode instantly
   // OR move cursor inside and convert automatically?
 
-
   /**
-   * 
+   *
    * Check if inside a Notion block equation for \begin{**} & \end{**}
-   *  
-   */ 
+   *
+   */
   if (node.parentElement?.className === Token.keyword) {
     const blockNode = node.parentNode?.parentNode;
     const text = blockNode?.textContent;
 
     if (text?.match(re1) || text?.match(re2) || text?.match(re3)) {
       const childNodes = [...blockNode!.childNodes!];
-  
+
       // @ts-ignore get the "token keyword" node
       const indexEnvName = childNodes.indexOf(node.parentNode);
       // make sure it's a begin
       // @ts-ignore
-      const isBegin = childNodes[indexEnvName-2].className === Token.functionSelector && childNodes[indexEnvName-2].textContent === "\\begin";
+      const isBegin =
+        (childNodes[indexEnvName - 2] as Element).className === Token.functionSelector &&
+        childNodes[indexEnvName - 2].textContent === "\\begin";
 
       if (isBegin) {
         const envName = node.textContent ?? node.parentNode?.textContent;
         const envNodeCopy = node.cloneNode();
         // find the next "token function selector" that doesn't already have a matching closer
         // const childNodesAfterBegin = childNodes.slice(idx);
-        // remove matching closers 
+        // remove matching closers
         const indexEnd = childNodes.findIndex((value, index, object) => {
-          return
-          // make sure it occurs after the \begin class 
-          index > indexBegin &&
-          // it starts with "\end"
-          // @ts-ignore
-          value.className === Token.functionSelector && value.textContent === "\\end" &&
-          // the next thing is "{"
-          // @ts-ignore
-          (object[index+1].className === Token.punctuation) &&
-          // followed by either envName} or {}
-          // @ts-ignore
-          ((object[index+2].className === Token.keyword) || object[index+2].className === Token.punctuation)) &&
-          // ignore it if it already has a matching begin tag which comes after the current begin tag and before current end tag
-          // 
-          !( (object[index+2].className === Token.keyword)  &&  
-          // join the whole thing as a string
-          childNodes.slice(indexBegin, index+2).map((node) => node.textContent).join("").includes(`\\begin{${object[index+2].textContent}}`)
-          )
-        }
-  
+          return (
+            index > indexEnvName &&
+            (value as Element).className === Token.functionSelector &&
+            value.textContent === "\\end" &&
+            (object[index + 1] as Element).className === Token.punctuation &&
+            ((object[index + 2] as Element).className === Token.keyword ||
+              (object[index + 2] as Element).className === Token.punctuation) &&
+            !(
+              (object[index + 2] as Element).className === Token.keyword &&
+              childNodes
+                .slice(indexEnvName, index + 2)
+                .map((node) => node.textContent)
+                .join("")
+                .includes(
+                  `\\begin{${(object[index + 2] as Element).textContent}}`
+                )
+            )
+          );
+        });
+
         // check if the next thing is {} or {envName}
         if (childNodes[indexEnd]) {
-  
         }
       }
-      
-      
     }
     //   // empty \end{}
     //   // @ts-ignore
@@ -138,20 +136,16 @@ function onKeyUp(event: KeyboardEvent): void {
     // const envName = inBlockEqKeyword ? node.textContent! : undefined;
   }
 
-
   // TODO check inline math mode!
   /**
    *  replace latex env name for \begin{**} -> \end{**}
-   */ 
-  if (
-    node.textContent?.slice(0, offset).match(/\\begin{[a-z]*$/i)
-  ) {
-    
+   */
+  if (node.textContent?.slice(0, offset).match(/\\begin{[a-z]*$/i)) {
     const text = node?.textContent;
     if (text) {
       if (text.match(re1)) {
         // \begin{**} \end{} -> fill up \end{}
-          node.textContent = text?.replace(re1, "\\begin{$1}$2\\end{$1}");
+        node.textContent = text?.replace(re1, "\\begin{$1}$2\\end{$1}");
       } else if (text.match(re2)) {
         // \begin{**} \end{**} -> make \end{} match
         node.textContent = text?.replace(re2, "\\begin{$1}$2\\end{$1}");
